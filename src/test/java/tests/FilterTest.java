410bb5c9956dd7050214e93a15dde67610eb27bd
KhattabMennat <khattab.mennat-exter@deutschebahn.com>
2020-11-08 13:28:41 +0100

    new testng pom

--- src/test/java/tests/FilterTest.java
+++ src/test/java/tests/FilterTest.java
@@ -3,16 +3,21 @@ package tests;
 import org.testng.annotations.Test;
 import pages.FilterAnw;
 import pages.HomePage;
+import pages.UserLogin;
 
 import java.util.concurrent.TimeUnit;
 
 public class FilterTest extends TestBase{
 
     FilterAnw FilterObj;
+    UserLogin loginObj;
 
         @Test
         public void Filteranwenden() throws InterruptedException {
 
+            loginObj = new UserLogin(driver);
+            loginObj.Login("user","test");
+            Thread.sleep(6000);
             FilterObj =  new FilterAnw(driver);
             Thread.sleep(6000);
             FilterObj.useFilter("80");
